8d7ca8b356c2eb94a552f4387d77f3a8f4597160
AnupJoseph <43500348+AnupJoseph@users.noreply.github.com>
2024-08-17 07:04:41 +0000

    Add the initial mongo connect and setup

---
+++ src/utils/connect.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import config from "config";
+
+async function connect() {
+  const dbUri = config.get<string>("dbUri");
+  try {
+    await mongoose.connect(dbUri);
+  } catch (error) {
+    console.error("Could not connect to db");
+    process.exit(1);
+  }
+}
